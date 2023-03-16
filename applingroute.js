ecd257f9dee1e719c2f791b98bf075b44d3eaa46
HossamDaCyborg <hossamhamdy1482002@gmail.com>
2023-03-16 03:59:18 +0200

    stilldontknow

--- applingroute.js
+++ applingroute.js
@@ -13,30 +13,88 @@ for(let link of jobLinks){
         if(newDoc){
             let inpElements = newDoc.querySelectorAll('input,select,textarea');
             if(inpElements.length){
+                let file = 0
                 for(let element of inpElements){
+                    let itsId = element.id ?element.id:'';
+                    let itslabel = itsId ?  newDoc.querySelector(`[for="${itsId}"]`) :'';
+                    // represents the element object will be checked and pushed to the final results array
+                    let it ={}
+                    // red flags will be increased at any case of unsatsfyible requirement and will be checked for at yhe end of the loop if its not 0 it will break
+                    let redFlags = 0
+                    
                     switch (element.tagName) {
                         case 'INPUT':
                             switch (element.type) {
                                 case 'text':
-                                    let itsValue = element.value
-                                    let itsId = element.id
-                                    let itslabel = newDoc.querySelector(`[for="${itsId}"]`)
                                     if(itslabel)
                                     {
-                                        let itsValue = itslabel.innerHTML
-                                        let isItRequired = element.required
-                                        let isIt = !!isItRequired
-                                        let it = {}
-                                        it.itsValue = 'text'
-                                        it['requireed'] = isIt
-                                        finalREsults.push(it)
+                                        
+                                        if(element.value){
+                                            it[`${itslabel.innerHTML}`] = element.value
+                                        }
+                                        else {
+                                            it[`${itslabel.innerHTML}`] = 'default'
+                                            element.value = 'default'
+                                        }
+                                        
                                     }
-                                    
-                                    
+                                    if(!element.value){
+                                        element.value='default'
+                                    }
                                     break;
+                                case 'file':
+                                        if(itslabel)
+                                        {
+                                            
+                                            it[`${itslabel.innerHTML}`] = 'file'
+                                            if(!file >0){
+                                                document.querySelector('[aria-label="Choose Resume"]').click()
+                                                file++;
+                                            }
+                                            
+                                        }
+                                        else{
+                                            if(!file >0){
+                                                document.querySelector('[aria-label="Choose Resume"]').click()
+                                                file++;
+                                            }
+                                           
+
+                                        }
+                                        
+                                        
+                                    break; 
+                                case 'checkbox':
+                                        if(itslabel)
+                                        {
+                                            
+                                            it[`${itslabel.innerHTML}`] = 'checkbox'
+                                            
+                                            
+                                        }
+                                        if(!element.checked){element.click()}
+                                        
+                                        
+                                    break;
+                                case 'radio':
+                                        if(itslabel)
+                                        {
+                                            
+                                            it[`${itslabel.innerHTML}`] = 'checkbox'
+                                            
+                                            
+                                        }
+                                        if(!element.checked){element.click()}
+                                        
+                                        
+                                    break;   
+
+
                             
                                 default:
+                                    redFlags ++;
                                     break;
+                                    
                             }
                             
                             break;
